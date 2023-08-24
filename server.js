39665a5657b0c109fd994b803c70bfd65792d3b4
hardikgupta2003 <hardikgupta5172@gmail.com>
2023-08-24 23:43:19 +0530

    added .get

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const app= express() ;
 app.listen(3000,()=>{
     console.log("My server started at port no.3000")
 })
+
+app.get('/',(request,response)=>{
+    response.send("helloo this is working")
+})
